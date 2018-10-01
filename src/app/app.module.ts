7ee5b07afc9ed780789f6f3353efbb360a176df5
HamzaMoummid <hamza.moummid@gmail.com>
2018-10-01 23:07:39 +0200

    tst

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CurrentComponent } from './current/current.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { weatherRouting } from './weather.routing';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CurrentComponent,
+    ForecastComponent
+  ],
+  imports: [
+    BrowserModule,
+    weatherRouting
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
